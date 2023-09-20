53f6e1b40b8041c790ac5a898a9d515032fa957b
www42 <tjaekel0815@gmail.com>
2023-09-20 21:58:11 +0200

    Add monitoring to peering

--- HubAndSpoke/Peering.ps1
+++ HubAndSpoke/Peering.ps1
@@ -4,9 +4,10 @@
 #  - removes old (disconnected) peerings
 #  - creates new peerings
 
-$hub    = Get-AzVirtualNetwork -Name 'vnet-hub'                  -ResourceGroupName 'rg-hub'
-$hybrid = Get-AzVirtualNetwork -Name 'vnet-hybrididentity'       -ResourceGroupName 'rg-hybrididentity'
-$nested = Get-AzVirtualNetwork -Name 'vnet-nestedvirtualization' -ResourceGroupName 'rg-nestedvirtualization'
+$hub        = Get-AzVirtualNetwork -Name 'vnet-hub'                  -ResourceGroupName 'rg-hub'
+$hybrid     = Get-AzVirtualNetwork -Name 'vnet-hybrididentity'       -ResourceGroupName 'rg-hybrididentity'
+$nested     = Get-AzVirtualNetwork -Name 'vnet-nestedvirtualization' -ResourceGroupName 'rg-nestedvirtualization'
+$monitoring = Get-AzVirtualNetwork -Name 'vnet-monitoring'           -ResourceGroupName 'rg-monitoring'
 
 
 # --- Delete old (disconnected) peerings ---------------------------------------------
@@ -14,18 +15,25 @@ Get-AzVirtualNetworkPeering -VirtualNetworkName $hub.Name -ResourceGroupName 'rg
 Get-AzVirtualNetworkPeering -VirtualNetworkName $hub.Name -ResourceGroupName 'rg-hub' | Remove-AzVirtualNetworkPeering -Force
 
 
-# --- Hub <--> Hybrid ----------------------------------------------------------------
+# --- Hub <--> Hybrid (1) ------------------------------------------------------------
 Add-AzVirtualNetworkPeering -Name 'peer-hub-hybrid' -VirtualNetwork $hub    -RemoteVirtualNetworkId $hybrid.Id -AllowForwardedTraffic -AllowGatewayTransit
 Add-AzVirtualNetworkPeering -Name 'peer-hybrid-hub' -VirtualNetwork $hybrid -RemoteVirtualNetworkId $hub.Id    -AllowForwardedTraffic #-UseRemoteGateways
 
-# --- Hub <--> Nested ----------------------------------------------------------------
+# --- Hub <--> Nested (2) ------------------------------------------------------------
 Add-AzVirtualNetworkPeering -Name 'peer-hub-nested' -VirtualNetwork $hub    -RemoteVirtualNetworkId $nested.Id -AllowForwardedTraffic -AllowGatewayTransit
 Add-AzVirtualNetworkPeering -Name 'peer-nested-hub' -VirtualNetwork $nested -RemoteVirtualNetworkId $hub.Id    -AllowForwardedTraffic #-UseRemoteGateways
 
+# --- Hub <--> Monitoring (3) --------------------------------------------------------
+Add-AzVirtualNetworkPeering -Name 'peer-hub-monitoring' -VirtualNetwork $hub        -RemoteVirtualNetworkId $monitoring.Id -AllowForwardedTraffic -AllowGatewayTransit
+Add-AzVirtualNetworkPeering -Name 'peer-monitoring-hub' -VirtualNetwork $monitoring -RemoteVirtualNetworkId $hub.Id        -AllowForwardedTraffic #-UseRemoteGateways
+
+
+
 # Show
-Get-AzVirtualNetworkPeering -VirtualNetworkName $hub.Name    -ResourceGroupName 'rg-hub'                  | ft Name,PeeringState,AllowForwardedTraffic,AllowGatewayTransit,UseRemoteGateways
-Get-AzVirtualNetworkPeering -VirtualNetworkName $hybrid.Name -ResourceGroupName 'rg-hybrididentity'       | ft Name,PeeringState,AllowForwardedTraffic,AllowGatewayTransit,UseRemoteGateways
-Get-AzVirtualNetworkPeering -VirtualNetworkName $nested.Name -ResourceGroupName 'rg-nestedvirtualization' | ft Name,PeeringState,AllowForwardedTraffic,AllowGatewayTransit,UseRemoteGateways
+Get-AzVirtualNetworkPeering -VirtualNetworkName $hub.Name        -ResourceGroupName 'rg-hub'                  | ft Name,PeeringState,AllowForwardedTraffic,AllowGatewayTransit,UseRemoteGateways
+Get-AzVirtualNetworkPeering -VirtualNetworkName $hybrid.Name     -ResourceGroupName 'rg-hybrididentity'       | ft Name,PeeringState,AllowForwardedTraffic,AllowGatewayTransit,UseRemoteGateways
+Get-AzVirtualNetworkPeering -VirtualNetworkName $nested.Name     -ResourceGroupName 'rg-nestedvirtualization' | ft Name,PeeringState,AllowForwardedTraffic,AllowGatewayTransit,UseRemoteGateways
+Get-AzVirtualNetworkPeering -VirtualNetworkName $monitoring.Name -ResourceGroupName 'rg-monitoring'           | ft Name,PeeringState,AllowForwardedTraffic,AllowGatewayTransit,UseRemoteGateways
 
 # Set 'UseRemoteGateways'
 $hybridToHub = Get-AzVirtualNetworkPeering -VirtualNetworkName $hybrid.Name -ResourceGroupName 'rg-hybrididentity'
