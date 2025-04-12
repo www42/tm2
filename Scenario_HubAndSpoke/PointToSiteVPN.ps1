184d3c5e0ab2af1123ed9dd953a7e76f50eb69ab
www42 <tjaekel0815@gmail.com>
2025-04-12 12:43:12 +0200

    Add dns resol

--- Scenario_HubAndSpoke/PointToSiteVPN.ps1
+++ Scenario_HubAndSpoke/PointToSiteVPN.ps1
@@ -7,6 +7,9 @@
 
 # Download and install VPN Client Software (Azure CLI)
 # -------------------------------------------------------------------
+# Should be 
+#   $rgName = 'rg-hub'
+#   $gatewayName = 'vgw-hub'
 $rgName
 $gatewayName
 
@@ -16,6 +19,9 @@ $uri = az network vnet-gateway vpn-client generate `
     --output tsv
 
 $vpnZipPath = "$env:HOMEPATH\Downloads"
+# Remove the old VPN client
+Remove-Item -Path $vpnZipPath\VpnClient -Recurse -Force
+Remove-Item -Path $vpnZipPath\VpnClient.zip -Force
 Invoke-RestMethod -Uri $uri -OutFile $vpnZipPath\VpnClient.zip
 dir $vpnZipPath\VpnClient.zip
 Expand-Archive -Path $vpnZipPath\VpnClient.zip -DestinationPath $vpnZipPath\VpnClient
@@ -29,27 +35,89 @@ cmd.exe /C "start ms-settings:network-vpn"
 
 # Test connectivity
 Get-NetIPConfiguration | Format-Table InterfaceIndex,InterfaceAlias,InterfaceDescription,IPv4Address
-Get-NetRoute -AddressFamily IPv4 -DestinationPrefix '10.0.0.0/16','10.1.0.0/16'
 Get-NetRoute -AddressFamily IPv4 -DestinationPrefix '10.0.0.0/16','10.1.0.0/16','10.2.0.0/16','10.3.0.0/16'
 
-#   Test 1:   dc1 (10.1.0.200), svr1 (10.3.0.4)
+#   Test  dc1 (10.1.0.200), svr1 (10.3.0.4)
 Test-NetConnection 10.1.0.200 -Port 3389
 mstsc -v 10.1.0.200    # Remove RD Gateway!
 
 
-#   Test 2:   private EP storage (10.1.0.4)
-Test-NetConnection 10.1.0.4 -Port 443
-# DNS --> Does not work
-Resolve-DnsName -Name private69118.blob.core.windows.net
-Test-NetConnection -ComputerName private69118.blob.core.windows.net -Port 443 -InformationLevel 'Detailed'
-Invoke-WebRequest -Uri https://private69118.blob.core.windows.net/web/Hello.html -UseBasicParsing | % content
-
-
-
-
+# Clean up
+# -------------------------------------------------------------------
 # Disconnect and remove VPN
 cmd.exe /C "start ms-settings:network-vpn"
 
 # Remove VPN Client
 Remove-Item -Path $vpnZipPath\VpnClient -Recurse -Force
 Remove-Item -Path $vpnZipPath\VpnClient.zip -Force
+
+
+
+
+# Access Private EP 10.1.0.4 connected to blob service of storage account 'private69118'
+# --------------------------------------------------------------------------------------
+
+#   *** From dc1 *****
+# We are in the same Vnet as the private EP [10.1.0.200]
+Get-NetIPConfiguration
+
+# Is Private EP reachable by IP address? [Yes]
+Test-NetConnection -ComputerName 10.1.0.4 -Port 443
+
+# Is blob accessible by IP address? [No, certificate error]
+Invoke-WebRequest -Uri https://10.1.0.4/txt/Hello.txt -UseBasicParsing | % content
+
+# Name resolution private69118.blob.core.windows.net --> 10.1.0.4 available? [Yes via linked private DNS zone]
+Resolve-DnsName -Name private69118.blob.core.windows.net -Type A
+
+# -------------------------------------------------------
+# Private EP is already integrated with private DNS zone.
+# -------------------------------------------------------
+
+# Blob is accessible
+Test-NetConnection -ComputerName private69118.blob.core.windows.net -Port 443
+Invoke-WebRequest -Uri https://private69118.blob.core.windows.net/txt/Hello.txt -UseBasicParsing | % content
+
+
+
+
+#   *** From svr1 *****
+# We are in a different Vnet as the private EP [10.3.0.4]
+Get-NetIPConfiguration
+
+# Is Private EP reachable by IP address? [Yes]
+Test-NetConnection -ComputerName 10.1.0.4 -Port 443
+
+# Is blob accessible by IP address? [No, certificate error]
+Invoke-WebRequest -Uri https://10.1.0.4/txt/Hello.txt -UseBasicParsing | % content
+
+# Name resolution private69118.blob.core.windows.net --> 10.1.0.4 available? 
+#   [No, there is no linked private DNS zone, public DNS yields public IP address]
+Resolve-DnsName -Name private69118.blob.core.windows.net -Type A
+
+# --------------------------------
+# Link Vnet to private DNS zone!
+# --------------------------------
+
+# Now it works
+Resolve-DnsName -Name private69118.blob.core.windows.net -Type A
+Test-NetConnection -ComputerName private69118.blob.core.windows.net -Port 443
+Invoke-WebRequest -Uri https://private69118.blob.core.windows.net/txt/Hello.txt -UseBasicParsing | % content
+
+
+
+
+#   *** From Notebook VPN Client *****
+Test-NetConnection -ComputerName 10.1.0.4 -Port 443
+Resolve-DnsName -Name private69118.blob.core.windows.net -Type A
+
+# -------------------------------------------------------------
+# Edit c:\windows\system32\drivers\etc\hosts   as Administrator
+#      10.1.0.4  private69118.blob.core.windows.net
+# --------------------------------------------------------------
+
+# Now it works
+Resolve-DnsName -Name private69118.blob.core.windows.net -Type A
+Test-NetConnection -ComputerName private69118.blob.core.windows.net -Port 443
+Invoke-WebRequest -Uri https://private69118.blob.core.windows.net/txt/Hello.txt -UseBasicParsing | % content
+
--- Scenario_HubAndSpoke/SpokeToSpokeRouting.ps1
+++ Scenario_HubAndSpoke/SpokeToSpokeRouting.ps1
@@ -2,23 +2,56 @@
 # This script
 #  - creates UDRs for spoke-to-spoke routing
 # --------------------------------------------------------------------------
-$hubPrefix        = '10.0.0.0/16'
-$hybridPrefix     = '10.1.0.0/16'
-$nestedPrefix     = '10.2.0.0/16'
-$monitoringPrefix = '10.3.0.0/16'
 
+
+# Get the virtual networks
+# --------------------------------------------------------------------------
 $hub        = Get-AzVirtualNetwork -Name 'vnet-hub'                  -ResourceGroupName 'rg-hub'
 $hybrid     = Get-AzVirtualNetwork -Name 'vnet-hybrididentity'       -ResourceGroupName 'rg-hybrididentity'
 $nested     = Get-AzVirtualNetwork -Name 'vnet-nestedvirtualization' -ResourceGroupName 'rg-nestedvirtualization'
 $monitoring = Get-AzVirtualNetwork -Name 'vnet-monitoring'           -ResourceGroupName 'rg-monitoring'
 
+# Should be
+#    $hubPrefix        = '10.0.0.0/16'
+#    $hybridPrefix     = '10.1.0.0/16'
+#    $nestedPrefix     = '10.2.0.0/16'
+#    $monitoringPrefix = '10.3.0.0/16'
+$hubPrefix        = $hub        | % AddressSpace | % { $_.AddressPrefixes }
+$hybridPrefix     = $hybrid     | % AddressSpace | % { $_.AddressPrefixes }
+$nestedPrefix     = $nested     | % AddressSpace | % { $_.AddressPrefixes }
+$monitoringPrefix = $monitoring | % AddressSpace | % { $_.AddressPrefixes }
+
+
 
 # Check peerings
 # --------------------------------------------------------------------------
-#       * Hub peerings to spokes should have 'AllowGatewayTransit' set to 'True'
-#       * Spoke peerings to hub  should have 'UseRemoteGateways' set to 'True'
+#   * Hub peerings to spokes should have 'AllowGatewayTransit' set to 'True'
+#   * Spoke peerings to hub  should have 'UseRemoteGateways' set to 'True'
 #
-#    --> Peering.ps1
+#   To correct --> Peering.ps1
+Get-AzVirtualNetworkPeering -VirtualNetworkName $hub.Name        -ResourceGroupName 'rg-hub'                  | ft Name,PeeringState,AllowForwardedTraffic,AllowGatewayTransit,UseRemoteGateways
+Get-AzVirtualNetworkPeering -VirtualNetworkName $hybrid.Name     -ResourceGroupName 'rg-hybrididentity'       | ft Name,PeeringState,AllowForwardedTraffic,AllowGatewayTransit,UseRemoteGateways
+Get-AzVirtualNetworkPeering -VirtualNetworkName $nested.Name     -ResourceGroupName 'rg-nestedvirtualization' | ft Name,PeeringState,AllowForwardedTraffic,AllowGatewayTransit,UseRemoteGateways
+Get-AzVirtualNetworkPeering -VirtualNetworkName $monitoring.Name -ResourceGroupName 'rg-monitoring'           | ft Name,PeeringState,AllowForwardedTraffic,AllowGatewayTransit,UseRemoteGateways
+
+
+# Show UDRs
+# --------------------------------------------------------------------------
+$vNet = $hybrid
+$routeTableName = ($vnet.Subnets[0].RouteTableText | ConvertFrom-Json | % Id).split('/')[-1]
+Get-AzRouteTable -Name $routeTableName | % RoutesText | ConvertFrom-Json | % { $_.AddressPrefix }
+
+
+$vNet = $nested
+$routeTableName = ($vnet.Subnets[0].RouteTableText | ConvertFrom-Json | % Id).split('/')[-1]
+Get-AzRouteTable -Name $routeTableName | % RoutesText | ConvertFrom-Json | % { $_.AddressPrefix }
+
+
+$vNet = $monitoring
+$routeTableName = ($vnet.Subnets[0].RouteTableText | ConvertFrom-Json | % Id).split('/')[-1]
+Get-AzRouteTable -Name $routeTableName | % RoutesText | ConvertFrom-Json | % { $_.AddressPrefix }
+
+
 
 
 # UDR Hybrid --> Nested, Monitoring
