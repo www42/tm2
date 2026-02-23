0fa87bb78eec38cd5872c32529f7da689be8027c
www42 <tjaekel0815@gmail.com>
2026-02-23 23:39:25 +0100

    Add delete

--- Kubernetes/DEPLOY.ps1
+++ Kubernetes/DEPLOY.ps1
@@ -1,1 +1,11 @@
+dir 'Kubernetes/supermario.yaml'
+
+kubectl config current-context
+
 kubectl apply -f 'Kubernetes/supermario.yaml'
+
+kubectl get pods
+kubectl get service
+
+
+kubectl delete -f 'Kubernetes/supermario.yaml'
