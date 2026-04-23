a76f75e51f5e39f1789818f3b450e2cb79c1285d
www42 <tjaekel0815@gmail.com>
2026-04-23 08:35:26 +0200

    Add vote.yaml

--- Kubernetes/DEPLOY.ps1
+++ Kubernetes/DEPLOY.ps1
@@ -1,11 +1,30 @@
-dir 'Kubernetes/supermario.yaml'
+$rgName= 'rg-kubernetes'
 
+az aks list --resource-group $rgName --query "[].{name:name,kubernetesVersion:kubernetesVersion,fqdn:fqdn}" -o table
+$clusterName = 'kube1'
+
+az aks get-credentials --resource-group $rgName --name $clusterName
+
+kubectl config get-clusters
+kubectl config get-contexts 
 kubectl config current-context
 
+kubectl get nodes
+kubectl get pods
+kubectl get pods --all-namespaces
+
+# Super Mario
+# -----------------
+dir Kubernetes/supermario.yaml
 kubectl apply -f 'Kubernetes/supermario.yaml'
 
 kubectl get pods
 kubectl get service
 
-
 kubectl delete -f 'Kubernetes/supermario.yaml'
+
+# Azure Vote --> Does not work
+# ----------------------------
+dir Kubernetes/azure-vote.yaml
+kubectl apply  -f 'Kubernetes/azure-vote.yaml'
+kubectl delete -f 'Kubernetes/azure-vote.yaml'
